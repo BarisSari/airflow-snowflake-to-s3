a540c244eede95d0d00a083a83d08e4806788336
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-01 23:12:05 +0300

    snowflake_dag added

--- helper.py
+++ helper.py
@@ -1,6 +1,18 @@
-import airflow.hooks.S3_hook
+from airflow.contrib.hooks import snowflake_hook
+from airflow.hooks import S3_hook
+
+
+def fetch_data_from_snowflake():
+    hook = snowflake_hook.SnowflakeHook("snowflake_conn")
+    conn = hook.get_conn()
+    with conn.cursor() as cursor:
+        cursor.execute('USE DATABASE MY_AUDIT_DB;')
+        cursor.execute("SHOW roles")
+        print(cursor.fetchall())
+    # check data is correct or not
+    # create csv and return it
 
 
 def upload_file_to_s3_with_hook(filename, key, bucket_name):
-    hook = airflow.hooks.S3_hook.S3Hook('aws_s3_conn')
+    hook = S3_hook.S3Hook('aws_s3_conn')
     hook.load_file(filename, key, bucket_name)
---
+++ snowflake_dag.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from helper import fetch_data_from_snowflake
+
+default_args = {
+    'owner': 'baris',
+    'start_date': datetime(2019, 1, 1),
+    'retry_delay': timedelta(minutes=5)
+}
+with DAG('snowflake_dag_test', default_args=default_args,
+         schedule_interval='@once') as dag:
+    start_task = DummyOperator(
+        task_id='start'
+    )
+
+    fetch_from_snowflake = PythonOperator(
+        task_id="fetch_from_snowflake",
+        python_callable=fetch_data_from_snowflake,
+        op_kwargs={},
+        dag=dag,
+    )
+
+    start_task >> fetch_from_snowflake
