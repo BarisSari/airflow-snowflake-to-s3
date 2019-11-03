9376ee7bcf8e72cf272a91c59af5de3867b0d2c6
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-03 17:11:33 +0300

    snowflake and s3 dags merged into one

--- dag.py
+++ dag.py
@@ -2,28 +2,34 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from helper import upload_file_to_s3_with_hook
+from helper import fetch_data_from_snowflake, upload_file_to_s3_with_hook
 
 default_args = {
     'owner': 'baris',
     'start_date': datetime(2019, 1, 1),
     'retry_delay': timedelta(minutes=5)
 }
-with DAG('S3_dag_test', default_args=default_args,
+with DAG('snowflake_to_s3', default_args=default_args,
          schedule_interval='@once') as dag:
+
     start_task = DummyOperator(
-        task_id='dummy_start'
+        task_id='start'
     )
 
-    upload_to_S3_task = PythonOperator(
+    fetch_from_snowflake = PythonOperator(
+        task_id="fetch_from_snowflake",
+        python_callable=fetch_data_from_snowflake,
+        op_kwargs={},
+        dag=dag,
+    )
+
+    upload_to_s3_task = PythonOperator(
         task_id='upload_file_to_s3',
         python_callable=upload_file_to_s3_with_hook,
         op_kwargs={
-            'filename': '/home/baris/Desktop/Untitled.csv',
-            'key': 'test.csv',
+            'directory': './tmp/',
             'bucket_name': 'import-snowflake',
         },
         dag=dag)
 
-    # Use arrows to set dependencies between tasks
-    start_task >> upload_to_S3_task
+    start_task >> fetch_from_snowflake >> upload_to_s3_task
--- helper.py
+++ helper.py
@@ -1,22 +1,44 @@
 import csv
+import os
 
 from airflow.contrib.hooks import snowflake_hook
 from airflow.hooks import S3_hook
+from scripts import sf_grant, sf_role
 
 
 def fetch_data_from_snowflake():
     hook = snowflake_hook.SnowflakeHook("snowflake_conn")
     conn = hook.get_conn()
+    roles = []
     with conn.cursor() as cursor:
         cursor.execute('USE DATABASE MY_AUDIT_DB;')
         cursor.execute("SHOW roles")
-        data = cursor.fetchall()
+        rec_set = cursor.fetchall()
+        for rec in rec_set:
+            roles.append(sf_role(rec[1], rec[9]))
+        for role in roles:
+            cursor.execute("SHOW GRANTS TO ROLE " + role.name)
+            grant_set = cursor.fetchall()
+            for cur_grant in grant_set:
+                role.add_grant(
+                    sf_grant(cur_grant[1], cur_grant[2], cur_grant[3]),
+                    roles)
 
     with open("./tmp/roles.csv", "w")as f:
         writer = csv.writer(f, delimiter=',')
-        writer.writerows(data)
+        for role in roles:
+            writer.writerows(','.join([role.name, role.comment]))
+
+    with open("./tmp/role_grants.csv", "w")as f:
+        for role in roles:
+            role.write_grants(role.name, 'ROOT', f)
 
 
-def upload_file_to_s3_with_hook(filename, key, bucket_name):
+def upload_file_to_s3_with_hook(directory, bucket_name):
     hook = S3_hook.S3Hook('aws_s3_conn')
-    hook.load_file(filename, key, bucket_name)
+    files = os.listdir(directory)
+    base_path = os.path.abspath(directory)
+    for file in files:
+        file_path = os.path.join(base_path+file)
+        key = file[:-3]
+        hook.load_file(file_path, key, bucket_name)
