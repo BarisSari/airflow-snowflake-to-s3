4a2014a01186e99d5abcca651bba93ed741867cf
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-03 14:12:26 +0300

    csv writing added

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import csv
+
 from airflow.contrib.hooks import snowflake_hook
 from airflow.hooks import S3_hook
 
@@ -8,9 +10,11 @@ def fetch_data_from_snowflake():
     with conn.cursor() as cursor:
         cursor.execute('USE DATABASE MY_AUDIT_DB;')
         cursor.execute("SHOW roles")
-        print(cursor.fetchall())
-    # check data is correct or not
-    # create csv and return it
+        data = cursor.fetchall()
+
+    with open("./tmp/roles.csv", "w")as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(data)
 
 
 def upload_file_to_s3_with_hook(filename, key, bucket_name):
