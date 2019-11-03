1152b3f9fd377697032ad82bb459b03598eb2196
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-04 01:14:24 +0300

    user and user grant classes created

--- helper.py
+++ helper.py
@@ -19,6 +19,9 @@ def fetch_data_from_snowflake():
     users = []
     with conn.cursor() as cursor:
         cursor.execute("USE DATABASE MY_AUDIT_DB;")
+        cursor.execute("USE ROLE ACCOUNTADMIN;")
+
+        # Queries for Roles and Role Grants
         cursor.execute("SHOW roles")
         rec_set = cursor.fetchall()
         for rec in rec_set:
@@ -27,16 +30,18 @@ def fetch_data_from_snowflake():
             cursor.execute("SHOW GRANTS TO ROLE " + role.name)
             grant_set = cursor.fetchall()
             for cur_grant in grant_set:
-                role.add_grant(Grant(cur_grant[1], cur_grant[2], cur_grant[3]), roles)
+                role.add_grant(RoleGrant(cur_grant[1], cur_grant[2], cur_grant[3]), roles)
+
+        # Queries for User and User Grants
         cursor.execute("SHOW users")
         user_set = cursor.fetchall()
         for user in user_set:
-            users.append(Role(user[1], user[9]))
+            users.append(User(user[0], user[1]))
         for user in users:
             cursor.execute("SHOW GRANTS TO USER " + user.name)
             grant_set = cursor.fetchall()
             for cur_grant in grant_set:
-                user.add_grant(Grant(cur_grant[1], cur_grant[2], cur_grant[3]), users)
+                user.add_grant(UserGrant(cur_grant[1], cur_grant[2], cur_grant[3], cur_grant[4]))
 
     with open(ROLES_PATH, "w") as f:
         writer = csv.writer(f, delimiter=",")
@@ -50,11 +55,11 @@ def fetch_data_from_snowflake():
     with open(USERS_PATH, "w") as f:
         writer = csv.writer(f, delimiter=",")
         for user in users:
-            writer.writerows(user.name + ',' + user.comment)
+            writer.writerows(user.name + ',' + user.created_on)
 
     with open(USER_GRANTS_PATH, "w") as f:
         for user in users:
-            user.write_grants(user.name, "ROOT", f)
+            user.write_grants(f)
 
 
 def upload_file_to_s3_with_hook(bucket_name):
@@ -68,7 +73,7 @@ def upload_file_to_s3_with_hook(bucket_name):
     TMP_DIRECTORY.cleanup()
 
 
-class Grant(object):
+class RoleGrant(object):
     def __init__(self, in_privilege, in_object_type, in_object_name):
         self.privilege = in_privilege
         self.object_type = in_object_type
@@ -111,3 +116,31 @@ class Role(object):
             )
         for cur_role in self.child_roles:
             cur_role.write_grants(root, current_path, file_handle)
+
+
+class UserGrant(object):
+    def __init__(self, role, granted_to, grantee_name, granted_by):
+        self.role = role
+        self.granted_to = granted_to
+        self.grantee_name = grantee_name
+        self.granted_by = granted_by
+
+
+class User(object):
+    def __init__(self, name, created_on):
+        self.name = name
+        self.created_on = str(created_on)
+        self.grants = set()
+
+    def add_grant(self, grant):
+        self.grants.add(grant)
+
+    def write_grants(self, file_handle):
+        for grant in self.grants:
+            print(
+                grant.role,
+                grant.granted_to,
+                grant.grantee_name,
+                grant.granted_by,
+                file=file_handle,
+            )
